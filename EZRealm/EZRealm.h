204111b4abf5a3947e57acaecc51c236a987cd9c
Sahand Raeisi <sahandraeisi1994@gmail.com>
2021-11-24 12:14:32 +0330

    Initial commit

---
+++ EZRealm/EZRealm.h
@@ -0,0 +1,18 @@
+//
+//  EZRealm.h
+//  EZRealm
+//
+//  Created by shndrs on 11/24/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EZRealm.
+FOUNDATION_EXPORT double EZRealmVersionNumber;
+
+//! Project version string for EZRealm.
+FOUNDATION_EXPORT const unsigned char EZRealmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EZRealm/PublicHeader.h>
+
+
