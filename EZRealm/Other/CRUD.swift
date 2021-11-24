d795cf0157ea4d56a83c42bbab2009ed72153a79
Sahand Raeisi <sahandraeisi1994@gmail.com>
2021-11-24 13:01:03 +0330

    Added CRUD

---
+++ EZRealm/Other/CRUD.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD.swift
+//  EZRealm
+//
+//  Created by shndrs on 11/24/21.
+//
+
+import Foundation
+
+public enum CRUD: UInt8 {
+    
+    case create
+    case read
+    case update
+    case delete
+    
+}
