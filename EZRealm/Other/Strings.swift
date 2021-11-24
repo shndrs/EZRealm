6ebf24c45a96e27c01e835b00331c7f196c9d4ad
Sahand Raeisi <sahandraeisi1994@gmail.com>
2021-11-24 13:01:53 +0330

    Added Strings

---
+++ EZRealm/Other/Strings.swift
@@ -0,0 +1,14 @@
+//
+//  Strings.swift
+//  EZRealm
+//
+//  Created by shndrs on 11/24/21.
+//
+
+import Foundation
+
+enum Strings: String {
+    
+    case updateError = "DBError: 'newItem' must has a value for update action"
+    
+}
